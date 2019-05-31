46bf1d42f0f4b058d058b97b69fda9272721650f
chessai <chessai1996@gmail.com>
2019-05-31 15:14:39 -0400

    finish api

--- src/Data/Primitive/Foreign.hs
+++ src/Data/Primitive/Foreign.hs
@@ -1,25 +1,68 @@
 --------------------------------------------------------------------------------
 
+{-# language BangPatterns #-}
+{-# language ForeignFunctionInterface #-}
 {-# language ScopedTypeVariables #-}
 {-# language TypeApplications #-}
 
 --------------------------------------------------------------------------------
 
 module Data.Primitive.Foreign
-  ( sizeOf, alignment
+  (
+    -- * Prim-Storable methods
+    sizeOf, alignment
   , peek, peekElemOff, peekByteOff
   , poke, pokeElemOff, pokeByteOff
 
+    -- * Memory allocation
+    -- ** Local allocation
   , alloca, F.allocaBytes, F.allocaBytesAligned
+    -- ** Dynamic allocation
   , malloc, F.mallocBytes
   , calloc, F.callocBytes
   , realloc, F.reallocBytes
-  , F.free
+  , F.free, F.finalizerFree
+
+    -- * Marshalling arrays
+    -- ** Allocation
+  , mallocArray, mallocArray0
+  , allocaArray, allocaArray0
+  , reallocArray, reallocArray0
+  , callocArray, callocArray0
+    -- ** Marshalling
+  , peekArray, peekArray0
+  , pokeArray, pokeArray0
+    -- ** Combined allocation and marshalling
+  , newArray, newArray0
+  , withArray, withArray0
+  , withArrayLen, withArrayLen0
+    -- ** Copying
+  , copyArray
+  , moveArray
+    -- ** Finding the length
+  , lengthArray0
+    -- ** Indexing
+  , advancePtr
+
+    -- * General marshalling utilities
+    -- ** Combined allocation and marshalling
+  , with
+  , new
+    -- ** Marshalling of Maybe values
+  , F.maybeNew
+  , F.maybeWith
+  , F.maybePeek
+    -- ** Haskellish interface to memcpy and memmove
+  , F.copyBytes
+  , F.moveBytes
+    -- ** Filling up memory areas with required values
+  , F.fillBytes
   ) where
 
 --------------------------------------------------------------------------------
 
 import Data.Coerce (coerce)
+import Data.Primitive.PrimArray
 import Data.Primitive.Types
 import Data.Void (Void)
 import GHC.Ptr
@@ -109,28 +152,161 @@ realloc ptr = castPtr <$> F.realloc @a @(PrimStorable b) ptr
 
 -- [Section: Foreign.Marshal.Array]
 
+-- | Allocate storage for the given number of elements of a storable type
+--   (like 'malloc', but for multiple elements).
 mallocArray :: forall a. Prim a => Int -> IO (Ptr a)
 mallocArray idx = castPtr <$> F.mallocArray @(PrimStorable a) idx
 
+-- | Like 'mallocArray', but add an extra position to hold a special
+--   termination element.
 mallocArray0 :: forall a. Prim a => Int -> IO (Ptr a)
 mallocArray0 idx = castPtr <$> F.mallocArray0 @(PrimStorable a) idx
 
+-- | Temporarily allocate space for the given number of elements
+--   (like 'alloca', but for multiple elements).
 allocaArray :: forall a b. Prim a => Int -> (Ptr a -> IO b) -> IO b
 allocaArray idx f = F.allocaArray idx (coerce f :: Ptr (PrimStorable a) -> IO b)
 
+-- | Like 'allocaArray', but add an extra position to hold a special
+--   termination element.
 allocaArray0 :: forall a b. Prim a => Int -> (Ptr a -> IO b) -> IO b
 allocaArray0 idx f = F.allocaArray0 idx (coerce f :: Ptr (PrimStorable a) -> IO b)
 
+-- | Adjust the size of an array.
 reallocArray :: forall a. Prim a => Ptr a -> Int -> IO (Ptr a)
 reallocArray ptr idx = castPtr <$> F.reallocArray @(PrimStorable a) (castPtr ptr) idx
 
+-- | Adjust the size of an array, including an extra position for the
+--   terminating element.
 reallocArray0 :: forall a. Prim a => Ptr a -> Int -> IO (Ptr a)
 reallocArray0 ptr idx = castPtr <$> F.reallocArray0 @(PrimStorable a) (castPtr ptr) idx
 
+-- | Like 'mallocArray', but allocated memory is filled with bytes of value zero.
 callocArray :: forall a. Prim a => Int -> IO (Ptr a)
 callocArray idx = castPtr <$> F.callocArray @(PrimStorable a) idx
 
+-- | Like 'mallocArray0', but allocated memory is filled with bytes of value zero.
 callocArray0 :: forall a. Prim a => Int -> IO (Ptr a)
 callocArray0 idx = castPtr <$> F.callocArray0 @(PrimStorable a) idx
+
+-- | Convert an array of given length into a Haskell 'PrimArray'.
+peekArray :: forall a. Prim a => Int -> Ptr a -> IO (PrimArray a)
+peekArray !sz ptr = if sz <= 0
+  then pure mempty
+  else do
+    marr <- newPrimArray sz
+    let go !ix = if ix < sz
+          then do
+            writePrimArray marr ix =<< peekElemOff ptr ix
+            go (ix + 1)
+          else pure ()
+    go 0
+    unsafeFreezePrimArray marr
+
+-- | Convert an array terminated by the given terminator into a Haskell
+-- 'PrimArray'.
+peekArray0 :: forall a. (Prim a, Eq a) => a -> Ptr a -> IO (PrimArray a)
+peekArray0 term ptr = lengthArray0 term ptr >>= \size ->
+  peekArray size ptr
+
+-- | Write the 'PrimArray' into memory at the given location.
+pokeArray :: forall a. Prim a => Ptr a -> PrimArray a -> IO ()
+pokeArray ptr arr = flip itraversePrimArray_ arr $ \ix atIx ->
+  pokeElemOff ptr ix atIx
+
+-- | Write the 'PrimArray' into memory and terminate the elements
+--   with a given terminating element.
+pokeArray0 :: forall a. Prim a => a -> Ptr a -> PrimArray a -> IO ()
+pokeArray0 term ptr arr =
+  let !sz = sizeofPrimArray arr
+   in flip itraversePrimArray_ arr $ \ix atIx ->
+     if ix == sz
+       then pokeElemOff ptr ix term
+       else pokeElemOff ptr ix atIx
+
+-- | Write a 'PrimArray' into a newly allocated, consecutive
+-- sequence of primitive values.
+newArray :: forall a. Prim a => PrimArray a -> IO (Ptr a)
+newArray arr = do
+  ptr <- mallocArray (sizeofPrimArray arr)
+  pokeArray ptr arr
+  pure ptr
+
+-- | Write a 'PrimArray' into a newly allocated, consecutive
+--   sequence of primitive values, where the end is fixed by
+--   the given terminating element.
+newArray0 :: forall a. Prim a => a -> PrimArray a -> IO (Ptr a)
+newArray0 term arr = do
+  ptr <- mallocArray0 (sizeofPrimArray arr)
+  pokeArray0 term ptr arr
+  pure ptr
+
+-- | Temporarily store a 'PrimArray' in memory.
+withArray :: forall a b. Prim a => PrimArray a -> (Ptr a -> IO b) -> IO b
+withArray arr = withArrayLen arr . const
+
+-- | Like 'withArray', but the action is also passed the size
+--   of the 'PrimArray'.
+withArrayLen :: forall a b. Prim a => PrimArray a -> (Int -> Ptr a -> IO b) -> IO b
+withArrayLen arr f = allocaArray len $ \ptr -> do
+  pokeArray ptr arr
+  f len ptr
+  where
+    !len = sizeofPrimArray arr
+
+-- | Like 'withArray', but a terminator indicates where the array ends.
+withArray0 :: forall a b. Prim a => a -> PrimArray a -> (Ptr a -> IO b) -> IO b
+withArray0 term arr = withArrayLen0 term arr . const
+
+-- | Like 'withArrayLen', but a terminator indicates where the array ends.
+withArrayLen0 :: forall a b. Prim a => a -> PrimArray a -> (Int -> Ptr a -> IO b) -> IO b
+withArrayLen0 term arr f = allocaArray0 len $ \ptr -> do
+  pokeArray0 term ptr arr
+  f len ptr
+  where
+    !len = sizeofPrimArray arr
+
+-- | Return the number of elements in an array, excluding
+--   the terminator
+lengthArray0 :: forall a. (Prim a, Eq a)
+  => a -- ^ terminating element
+  -> Ptr a
+  -> IO Int
+lengthArray0 term ptr = go 0
+  where
+    go !ix = peekElemOff ptr ix >>= \val ->
+      if val == term then pure ix else go (ix + 1)
+
+-- | Advance a pointer into an array by the given number of elements.
+advancePtr :: forall a. Prim a => Ptr a -> Int -> Ptr a
+advancePtr ptr !ix = ptr `plusPtr` (ix * sizeOf @a undefined)
+
+-- | Copy the given number of elements from the source array
+--   into the destination array; the memory regions /may not/ overlap.
+copyArray :: forall a. Prim a
+  => Ptr a -- ^ destination array
+  -> Ptr a -- ^ source array
+  -> Int -- ^ number of elements to copy
+  -> IO ()
+copyArray dest src size = F.copyBytes dest src (size * sizeOf @a undefined)
+
+-- | Copy the given number of elements from the source array
+--   into the destination array; the memory regions /may/ overlap.
+moveArray :: forall a. Prim a
+  => Ptr a -- ^ destination array
+  -> Ptr a -- ^ source array
+  -> Int -- ^ number of elements to copy
+  -> IO ()
+moveArray dest src size = F.moveBytes dest src (size * sizeOf @a undefined)
+
+--------------------------------------------------------------------------------
+
+-- [Section: Foreign.Marshal.Utils]
+
+with :: forall a b. Prim a => a -> (Ptr a -> IO b) -> IO b
+with val f = F.with (PrimStorable val) (coerce f :: Ptr (PrimStorable a) -> IO b)
+
+new :: forall a. Prim a => a -> IO (Ptr a)
+new val = castPtr <$> F.new (PrimStorable val)
 
 --------------------------------------------------------------------------------
