8bbf00a8cb2384870b35c5b122b08ebbc3c0bc65
chessai <chessai1996@gmail.com>
2019-05-31 11:57:43 -0400

    initial commit

---
+++ src/Data/Primitive/Foreign.hs
@@ -0,0 +1,5 @@
+module Data.Primitive.Foreign
+  (
+  ) where
+
+
--- src/PrimitiveFfi.hs
+++
@@ -1,6 +0,0 @@
-module PrimitiveFfi
-       ( someFunc
-       ) where
-
-someFunc :: IO ()
-someFunc = putStrLn ("someFunc" :: String)
